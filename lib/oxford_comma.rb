033da00e82ec2e81600651001b8a2b543128e6dc
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 22:48:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,9 @@ def oxford_comma(array)
   if array.size == 2
     return array.join(" and ")
   end
+  if array.size == 3
+    return array.join(",", " and ")
+  end
   if array.size > 3
     return array.join(",")
   end
