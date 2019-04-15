25d58a17b9beac5fd002126ee3ee9843676f43f9
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 22:55:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
   if array.size == 3
     element = array.pop
     newString = array.join(", ")
-    newString += " and #{element}"
+    newString += ", and #{element}"
     return newString
   end
   if array.size > 3
